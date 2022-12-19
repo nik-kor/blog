4527fb8a776676bc540d03654a0c207addf3740c
Nikita Korotkih <nikita.korotkih@gmail.com>
2022-12-19 22:17:20 +0100

    make it a list

--- bin/render.sh
+++ bin/render.sh
@@ -4,11 +4,10 @@ set -Eeuo pipefail
 list=""
 
 for i in $(ls tech | sort -r); do
-    list+="[$i](./tech/$i)\n";
+    list+="- [$i](./tech/$i)\n";
 done
 
 echo -e "\n[//]: # (RENDERED BASED ON README-template.md)" > README.md
-
 sed "s:%tech-posts-list%:$list:g" README-template.md >> README.md
 
 cat README.md
